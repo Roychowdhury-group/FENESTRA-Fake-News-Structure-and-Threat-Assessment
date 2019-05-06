ec46ebd92c97644c6327aad3a2b004fe42e17f65
Behnam <behnam.shahbazi40@gmail.com>
2019-05-06 07:46:42 -0700

    Added Flair Named Entity Recognizer. Unified NER tokenizer with our original (Strands') tokenizer, so we get a clean ner ranking. Produce flair_ner ranking.

--- entity_extractor.py
+++ entity_extractor.py
@@ -5,6 +5,13 @@ import nltk
 import re
 import math
 import time
+import ast
+from flair.embeddings import FlairEmbeddings, BertEmbeddings
+from flair.data import Sentence
+from flair.models import SequenceTagger
+from flair.embeddings import StackedEmbeddings
+import pickle
+
 
 class DataLoader:
     def __init__(self,
@@ -33,7 +40,9 @@ class DataLoader:
         #input_file_path = self.base_dir + input_file_name
         print("Loading ... ", path_to_file)
         if not os.path.isfile(path_to_file):
-            raise Exception("Unable to find the file at: ", path_to_file)
+            #raise Exception("Unable to find the file at: ", path_to_file)
+            print("Unable to find the file at: ", path_to_file)
+            return None
         try:
             df = pd.read_csv(path_to_file)
             print("Done. Number of records: ", len(df))
@@ -46,6 +55,8 @@ class DataLoader:
     def load_extractions(self, path_to_file=""):
         if path_to_file:
             self.df_extractions =  self.load_csv(path_to_file)
+            PIK = self.base_dir + "flair_res.pkl"
+            self.df_extractions["flair_res"] = self.load_from_pickle_object_list(PIK)
         else:
             self.df_extractions = self.load_csv(self.base_dir + self.df_extractions_name)
         return self.df_extractions
@@ -86,7 +97,10 @@ class EntityExtractor:
         self.df_ner_ranking_name = df_ner_ranking_name
         self.df_arg_ranking_name = df_arg_ranking_name
         self.df_extractions = self.data_loader.load_extractions()
-        self.df_ner_ranking = self.data_loader.load_ner_ranking()
+        #self.generate_or_load_flair_tags(overwrite=False)
+        #self.generate_or_load_ner_ranking(overwrite=False)
+        #self.df_ner_ranking = self.data_loader.load_ner_ranking()
+        #self.df_ner_ranking = self.generate_or_load_ner_ranking()
         if not os.path.exists(self.base_dir + self.df_arg_ranking_name):
             self.generate_arg_ranking(just_head_arg=True) # also sets self.df_arg_ranking variable
         else:
@@ -150,3 +164,108 @@ class EntityExtractor:
         self.df_arg_ranking = df_entity_rankings
         end_time = time.time()
         print("df_arg_ranking generation is done. Execution Time: ", (end_time-start_time)/60.0, " minutes.")
+
+    def save_to_pickle_object_list(self, data):
+        PIK = self.base_dir + "flair_res.pkl"
+        with open(PIK, "wb") as f:
+            pickle.dump(len(data), f)
+            for value in data:
+                pickle.dump(value, f)
+
+    def load_from_pickle_object_list(self, PIK):
+        data2 = []
+        with open(PIK, "rb") as f:
+            for _ in range(pickle.load(f)):
+                data2.append(pickle.load(f))
+        return data2
+
+    def generate_or_load_flair_tags(self, path_to_file="", overwrite = False):
+        if not path_to_file:
+            # file_name, file_name_extension = os.path.splitext(self.df_extraction_name)
+            path_to_file = self.base_dir + self.df_extraction_name #file_name #+ "_with_flair_res.csv"
+        if os.path.exists(path_to_file) and not overwrite:
+            print(path_to_file, "exists", "Loading ...")
+            return pd.read_csv(path_to_file)
+        else:
+            print("Setting up the stacked embedding -- Flair (backward/forward) + BERT")
+            tagger = SequenceTagger.load('ner-ontonotes-fast')
+
+            # init Flair embeddings
+            flair_forward_embedding = FlairEmbeddings('multi-forward')
+            flair_backward_embedding = FlairEmbeddings('multi-backward')
+
+            # init multilingual BERT
+            bert_embedding = BertEmbeddings('bert-base-cased')
+
+
+
+            # now create the StackedEmbedding object that combines all embeddings
+            stacked_embeddings = StackedEmbeddings(
+                embeddings=[flair_forward_embedding, flair_backward_embedding, bert_embedding])
+
+            print("Generating Flair Sentence Objects")
+            df_rels = self.df_extractions.copy()
+
+            def get_sentence_space_delimited(x):
+                annotation = ast.literal_eval(x["annotation"])
+                return " ".join([str(w) for w in annotation["words"]])
+
+            df_rels["flair_sentence"] = df_rels.apply(lambda x: Sentence(get_sentence_space_delimited(x)), axis=1)
+
+            print("Predicting Named Entities and Saving them to ", path_to_file)
+            import time
+            start_time = time.time()
+
+            res = tagger.predict(df_rels["flair_sentence"])
+
+            df_rels["flair_res"] = res
+            df_rels.to_csv(path_to_file)
+
+            self.save_to_pickle_object_list(res)
+            self.df_extractions = df_rels
+            end_time = time.time()
+            print("Tagger Prediction Done: ", end_time - start_time, "(seconds) - ", (end_time - start_time) / 60,
+                  "(min)")
+
+    def generate_or_load_ner_ranking(self, path_to_file="", overwrite=False):
+        if not path_to_file:
+            path_to_file = self.base_dir + self.df_ner_ranking_name
+        if os.path.exists(path_to_file) and not overwrite:
+            print(path_to_file, " exists. Loaded.")
+            return pd.read_csv(path_to_file)
+            # PIK = self.base_dir + "flair_res.pkl"
+            # df["flair_res"] = self.load_from_pickle_object_list(PIK)
+        else:
+            #if "flair_res" not in set(self.df_extractions.columns):
+            #    raise "Make sure you run named entity prediction task before aggregating the named entities. call generate_or_load_flair_tags method predict flair tags."
+            #df = pd.read_csv(path_to_file)
+
+
+            print("Generating ", path_to_file)
+            PIK = self.base_dir + "flair_res.pkl"
+            self.df_extractions["flair_res"] = self.load_from_pickle_object_list(PIK)
+            self.df_extractions["ner"] = self.df_extractions["flair_res"].apply(lambda x: x.get_spans('ner'))
+            print(self.df_extractions.iloc[0]["ner"])
+            print(self.df_extractions["ner"].head())
+            def get_list_of_span_dicts(row):
+                res = []
+                for span in row:
+                    res.append(span.to_dict())
+                return res
+
+            res = []
+            for ind, row in self.df_extractions["ner"].iteritems():
+                res += get_list_of_span_dicts(row)
+            print(len(res))
+            print(res[0])
+            df_ner_formatted = pd.DataFrame(res)
+
+            print(df_ner_formatted.head())
+            df_ner_sorted = df_ner_formatted.groupby(by=["text", "type"]).agg({'start_pos':'size', 'confidence':'mean'}) \
+                                                        .rename(columns={'start_pos':'count','confidence':'mean_confidence'}) \
+                                                         .sort_values(['count'], ascending=False).to_csv(path_to_file)
+
+            self.df_ner_ranking = df_ner_sorted
+            return self.df_ner_ranking
+
+
--- main.py
+++ main.py
@@ -1,10 +1,24 @@
+import torch
 from entity_extractor import *
+import ast
+import pandas as pd
 
+
+def test(ee):
+    a = ee.df_extractions.head()
+    a = a.iloc[0]
+    ann = a["annotation"]
+    ann = ast.literal_eval(ann)
+    print(ann)
+    print(ann.keys())
+    print(a["sentence"], len(a["sentence"].split(" ")))
+    words = " ".join(ann["words"])
+    print(words, len(ann["words"]))
 
 def main():
     ee = EntityExtractor(base_dir="/Users/behnam/Desktop/Behnam_Files/vwani_text_mining/RE_Behnam/data/FakeNews/bridgegate/small_accurate_set/results/rels_v2_with_pronoun/",
                             df_extraction_name="df_rels_with_ner.csv",
-                            df_ner_ranking_name="Entities_NER_Flair_Ranking_From_Sentences_with_conf.csv",
+                            df_ner_ranking_name="df_ner_ranking.csv",#"Entities_NER_Flair_Ranking_From_Sentences_with_conf.csv",
                             df_arg_ranking_name="df_arg_ranking.csv",
                             dataset_name="bridgegate")
 
@@ -16,7 +30,13 @@ def main():
     ent_extractor.generate_arg_ranking()
     df_arg_ranking = data_loader.generate_arg_ranking("df_argument_ranking_name")
     '''
-    print(ee.df_arg_ranking.head())
+    # print(ee.df_ner_ranking.head())
+    # print(ee.df_arg_ranking.head())
+    #test(ee)
+    ee.generate_or_load_flair_tags(overwrite=True)
+    ee.generate_or_load_ner_ranking(overwrite=True)
+
+
 
 
 if __name__ == '__main__':
