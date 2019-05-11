aefce963a4a51d13d42d2295aeb51ebc0a7106d3
Behnam <behnam.shahbazi40@gmail.com>
2019-05-11 13:34:13 -0700

    Testing the final entity ranking

--- main.py
+++ main.py
@@ -16,28 +16,18 @@ def test(ee):
     print(words, len(ann["words"]))
 
 def main():
-    ee = EntityExtractor(base_dir="/Users/behnam/Desktop/Behnam_Files/vwani_text_mining/RE_Behnam/data/FakeNews/bridgegate/small_accurate_set/results/rels_v2_with_pronoun/",
+    base_dir = "/Users/behnam/Desktop/Behnam_Files/vwani_text_mining/RE_Behnam/data/FakeNews/bridgegate/small_accurate_set/results/rels_v2_with_pronoun/"
+    ee = EntityExtractor(base_dir=base_dir,
                             df_extraction_name="df_rels_with_ner.csv",
                             df_ner_ranking_name="df_ner_ranking.csv",#"Entities_NER_Flair_Ranking_From_Sentences_with_conf.csv",
                             df_arg_ranking_name="df_arg_ranking.csv",
-                            dataset_name="bridgegate")
-
-    '''
-    data_loader = ent_extractor.dataLoader
-
-    df_extractions = data_loader.load_extractions()
-    df_ner_ranking = data_loader.load_ner_ranking()
-    ent_extractor.generate_arg_ranking()
-    df_arg_ranking = data_loader.generate_arg_ranking("df_argument_ranking_name")
-    '''
-    # print(ee.df_ner_ranking.head())
-    # print(ee.df_arg_ranking.head())
-    #test(ee)
-    ee.generate_or_load_flair_tags(overwrite=True)
-    ee.generate_or_load_ner_ranking(overwrite=True)
+                            dataset_name="bridgegate",
+                            load_all_data=False)
 
 
 
+    df_ent_final_ranking = ee.generate_or_load_final_ent_ranking(path_to_file= base_dir + "df_ent_final_ranking.csv", overwrite=False)
+    print(df_ent_final_ranking.head())
 
 
 if __name__ == '__main__':
